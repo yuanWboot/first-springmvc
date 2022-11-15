99f66db818cbadbb97e3d9449807cdeb19349e33
zy <1661996964@qq.com>
2022-11-15 18:40:25 +0800

    SpringMVC整合freemarker

--- src/main/java/com/zy/springmvc/controller/FreemarkerController.java
+++ src/main/java/com/zy/springmvc/controller/FreemarkerController.java
@@ -2,14 +2,17 @@ package com.zy.springmvc.controller;
 
 import com.zy.springmvc.entity.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/fm")
 public class FreemarkerController {
+    @GetMapping("/test")
+    // http://localhost/fm/test
     public ModelAndView showTest() {
-        ModelAndView mav = new ModelAndView();
+        ModelAndView mav = new ModelAndView("/test");
         User user = new User();
         user.setUsername("andy");
         mav.addObject("u",user);
